9c44bb4d0bdad4aec1acf6e7f1b01bd3056c0ab9
Isaac Ibanez Marquez <isaacibanez00@gmail.com>
2021-10-24 16:05:54 +0200

    Initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,30 @@
+public class FirstClass
+{
+
+    // En Java SIEMPRE hay que iniciar con este Main:
+
+
+
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello Wolrd");  // Para imprimir/ toda sentencia termina en [ ; ]
+
+        // Declaración de variable:
+
+        byte  edad;
+
+        // Iniciación de variable:
+        edad = 35;
+
+        System.out.println("La edad és: " + edad);
+
+        // Declaración e Iniciación de una Varaiable también se puede hacer en la misma linea:
+
+        byte edad2 = 45;
+
+        System.out.println("La edad2 és " + edad2);
+
+
+    }
+}
